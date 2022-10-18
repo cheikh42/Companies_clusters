ca561baffde0d9ffd85d6abd5b205864537ea11a
cheikh42 <cheikhmalaininec@gmail.com>
2022-10-18 17:28:23 +0200

    formatting

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from utils import siren_list, siren_mapping, tfidf_classification, result_CSV
 import numpy as np
-import logging 
+import logging
 
- # # Basic config ###
+# # Basic config ###
 logging.basicConfig(
     filename="logger.log", format="%(asctime)s - %(message)s", level=logging.INFO
 )
 
 logging.info("Start getting the sirens and companies' names")
 
-list_siren_companies,failed_list = siren_list()
+list_siren_companies, failed_list = siren_list()
 
 logging.info("End getting the sirens and companies' names")
 
@@ -27,8 +27,6 @@ logging.info("End clustering the companies without siren numbers using tf-idf")
 
 logging.info("Start creating the result file : 'dataset/result.csv'")
 
-result_CSV(dict_sirens,dict_failed)
+result_CSV(dict_sirens, dict_failed)
 
 logging.info("End creating the result file ")
-
-
--- utils.py
+++ utils.py
@@ -9,11 +9,10 @@ from sparse_dot_topn import awesome_cossim_topn
 
 BASE_URL = str(config("base_url"))
 FILE_PATH = str(config("file_path"))
-TRIAL  = str(config("trial"))
+TRIAL = str(config("trial"))
 
-def unsupervised_class(
-    doc_ids: list, names: list, train_type: str, nb_workers: int
-):
+
+def unsupervised_class(doc_ids: list, names: list, train_type: str, nb_workers: int):
     """
     unsupervised_class traines a model which clusters of the names.
     Saves it under the names unsupervised_model.
@@ -33,15 +32,15 @@ def unsupervised_class(
     )
 
     model.save("models/unsupervised_model")
-    
+
     return model
 
 
-def tfidf_classification(names : list):
+def tfidf_classification(names: list):
     """tfidf_classification clusters the names using the tf-idf scoring scheme.
 
     Args:
-        names (list): list of companies' names. 
+        names (list): list of companies' names.
 
     Returns:
         dict: dict of companies that contain similar names
@@ -51,36 +50,38 @@ def tfidf_classification(names : list):
     res_dict = dict()
 
     ### initialisation with empty lists
-    for name in names :
+    for name in names:
         res_dict[name] = []
 
     ### vectorization with tf-idf
     vectorizer = TfidfVectorizer(analyzer="word")
-    tfidf_matrix = vectorizer.fit_transform(names)    
-    
+    tfidf_matrix = vectorizer.fit_transform(names)
+
     ### compute the cosine similarity
-    cosine_matrix = awesome_cossim_topn(tfidf_matrix, tfidf_matrix.transpose(), names.size, 0.8)
+    cosine_matrix = awesome_cossim_topn(
+        tfidf_matrix, tfidf_matrix.transpose(), names.size, 0.8
+    )
     coo_matrix = cosine_matrix.tocoo()
-    
+
     with tqdm(
         total=len(coo_matrix.row),
         desc="{:30}".format("detecting similar words"),
     ) as pbar:
-        
+
         ### list of handled keys to avoid duplications
         tmp_keys = []
-        
+
         for i in zip(coo_matrix.row, coo_matrix.col):
-            
-            if i[1] not in tmp_keys: ### if a values is not amoung the handled keys
-                res_dict [names[ i[0] ] ].append( names[i[1]] )
+
+            if i[1] not in tmp_keys:  ### if a values is not amoung the handled keys
+                res_dict[names[i[0]]].append(names[i[1]])
                 tmp_keys.append(i[1])
                 pbar.update(1)
-    
+
     return res_dict
 
 
-def result_CSV(dict_sirens : dict, dict_failed : dict ) :
+def result_CSV(dict_sirens: dict, dict_failed: dict):
     """result_CSV creates a csv with two columns : the company name and the category ( an int ).
 
     Args:
@@ -91,23 +92,23 @@ def result_CSV(dict_sirens : dict, dict_failed : dict ) :
     categories = []
 
     tmp_category = 1
-    
+
     with tqdm(
-        total=len(dict_sirens.keys()) + len(dict_failed.keys() ) ,
+        total=len(dict_sirens.keys()) + len(dict_failed.keys()),
         desc="{:30}".format("detecting similar words"),
     ) as pbar:
-        
+
         ### file the companies with sirens
-        for k in dict_sirens.keys() : 
-            for name in dict_sirens[k] :
+        for k in dict_sirens.keys():
+            for name in dict_sirens[k]:
                 names.append(name)
                 categories.append(tmp_category)
             tmp_category += 1
             pbar.update()
-        
+
         ### file the companies with sirens
-        for k in dict_failed.keys() : 
-            for name in dict_failed[k] :
+        for k in dict_failed.keys():
+            for name in dict_failed[k]:
                 names.append(name)
                 categories.append(tmp_category)
             tmp_category += 1
@@ -119,13 +120,13 @@ def result_CSV(dict_sirens : dict, dict_failed : dict ) :
     dict_results["Category"] = categories
 
     ### create a frame
-    result_frame = pd.DataFrame( dict_results )
+    result_frame = pd.DataFrame(dict_results)
 
     ### create a csv
-    result_frame.to_csv('dataset/result.csv')
+    result_frame.to_csv("dataset/result.csv")
 
 
-def siren_list() :
+def siren_list():
     """siren_list generates a list of siren numbers and corresponding companies' names. This is done via French public API.
 
     Returns:
@@ -134,16 +135,16 @@ def siren_list() :
 
     ### load the dataset
     company_frame = pd.read_excel(FILE_PATH)
-    
+
     ### transform to a list
     company_list = company_frame["Raw name"].to_list()
 
     ### clean the lists
-    company_list = [str(i.replace("\"", "")) for i in company_list]
-    company_list = [str(i.replace("\'", "")) for i in company_list]
-    
+    company_list = [str(i.replace('"', "")) for i in company_list]
+    company_list = [str(i.replace("'", "")) for i in company_list]
+
     ### delete duplications if trial not equal to big
-    if TRIAL!="big" :
+    if TRIAL != "big":
         company_list = list(set(company_list))
 
     list_siren_companies = []
@@ -153,29 +154,27 @@ def siren_list() :
 
     ### we use tqdm to visualize the progression of the get requests
     with tqdm(
-            total=len(company_list),
-            desc="{:30}".format("getting the siren number"),
-        ) as pbar:
+        total=len(company_list),
+        desc="{:30}".format("getting the siren number"),
+    ) as pbar:
 
         for c in company_list:
 
-            tmp = c+"&page=1&per_page=1"
+            tmp = c + "&page=1&per_page=1"
 
-            try :
-                siren = requests.get(BASE_URL+tmp).json()["results"][0]["siren"]            
+            try:
+                siren = requests.get(BASE_URL + tmp).json()["results"][0]["siren"]
                 list_siren_companies.append([c, siren])
                 pbar.update(1)
 
-            except Exception as e :
+            except Exception as e:
                 failed_list.append(c)
                 pbar.update(1)
-    
-
 
     return list_siren_companies, failed_list
 
-    
-def siren_mapping(list_siren_companies : list) : 
+
+def siren_mapping(list_siren_companies: list):
     """siren_mapping creates a dictionnary of siren numbers (keys) and companies wwith these sirens (values).
 
     Args:
@@ -184,22 +183,17 @@ def siren_mapping(list_siren_companies : list) :
     Returns:
         dict: dictionnary of siren numbers and companies.
     """
-    
-    dict_sirens = dict()### dict of sirens (keys) and the companies having these sirens (values).
+
+    dict_sirens = (
+        dict()
+    )  ### dict of sirens (keys) and the companies having these sirens (values).
 
     ### initialize the dict with empty lists
-    for i in list_siren_companies :
+    for i in list_siren_companies:
         dict_sirens[i[1]] = []
 
     ### fil these empty lists
-    for i in list_siren_companies :
+    for i in list_siren_companies:
         dict_sirens[i[1]].append(i[0])
 
     return dict_sirens
-
-
-
-
-
-
-
